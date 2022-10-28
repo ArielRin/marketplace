5df16a7020ed5fccd92ac88c806fb81628dc5b6f
ArielRin <girlsmakingcrypto@gmail.com>
2022-10-28 17:38:54 +1100

    1st

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,15 +13,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb Marketplace with Next.JS</title>
+        <title>Sirens Treasure Trove</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's Marketplace with Next.JS To List Your NFTs For Sale, Accept Bids, and Buy NFTs"
+          content="In Game items, collectables and upgrades, List Your NFTs For Sale, Accept Bids, and Buy NFTs"
         />
         <meta
           name="keywords"
-          content="Thirdweb, Marketplace, NFT Marketplace Tutorial, NFT Auction Tutorial, How To Make OpenSea"
+          content="Sirens, Mermaid, Marketplace, NFT Marketplace, NFT Auction, Treasure, Trove, Dex, Dapp"
         />
       </Head>
       <Header />
